1f4fa50ffb6dbba8a57311289f4139ad37efd1ac
AashiqDina <98742552+AashiqDina@users.noreply.github.com>
2025-05-23 03:03:34 +0100

    Added Removal based on email and fixing names also Messier data

--- Main.py
+++ Main.py
@@ -6,8 +6,16 @@ if(len(files) != 0):
     for i in range(len(files)):
         if (files[i].lower().endswith('.csv')):
             DirtyFile = pd.read_csv("Input/" + files[i])
-            print(DirtyFile.duplicated())
-            print(DirtyFile.duplicated().sum())
+            # Fix Emails
+            DirtyFile['Email'] = DirtyFile['Email'].astype(str).str.lower() # Convert to lowercase
+            DirtyFile = DirtyFile[(DirtyFile['Email'].str.count('@') == 1) & (DirtyFile['Email'].str.contains(r'\.'))] # Checks if the email has exactly 1 @ and contains at least 1 . (for the end part like .com or .co.uk)
+            DirtyFile = DirtyFile.drop_duplicates(subset=['Email'], keep='first') # Removes any duplicate emails since emails should be unique
+
+            # Fix Names
+            DirtyFile['Name'] = DirtyFile['Name'].astype(str).str.lower() # Convert to lowercase
+            DirtyFile['Name'] = DirtyFile['Name'].str.strip() # remove spaces at the front or back
+            DirtyFile['Name'] = DirtyFile['Name'].str.replace(r'\s+', ' ', regex=True) # remove spaces between names (first and last names)
+            print(DirtyFile)
             
 else:
     print("There are no files in this folder")
